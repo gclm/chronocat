fd590c32c125e6d02e7de0c90b12373198838877
布丁w <ConBlueCut@outlook.com>
2023-10-26 16:27:39 +0800

    Update config.ts

--- packages/docs/config.ts
+++ packages/docs/config.ts
@@ -17,7 +17,7 @@ const presetConfig: Options = {
     routeBasePath: '/',
     sidebarPath: join(__dirname, 'sidebars.ts'),
     editUrl:
-      'https://github.com/chrononeko/chronocat/tree/master/packages/docs',
+      'https://github.com/BuDingOwO/chronocat/tree/master/packages/docs',
   },
 
   blog: {
@@ -35,7 +35,7 @@ const redocusaurusConfig: PresetOptions = {
     {
       id: 'openapi',
       spec: 'static/openapi.yaml',
-      url: 'https://chronocat.vercel.app/openapi.yaml',
+      url: 'https://chronocat-docs.vercel.app/openapi.yaml',
       route: '/develop/satori/reference/api',
       layout: {
         title: 'API 参考',
@@ -44,7 +44,7 @@ const redocusaurusConfig: PresetOptions = {
     {
       id: 'config',
       spec: 'static/openapi-config.json',
-      url: 'https://chronocat.vercel.app/openapi-config.json',
+      url: 'https://chronocat-docs.vercel.app/openapi-config.json',
     },
   ],
   theme: {
@@ -133,7 +133,7 @@ const themeConfig: ThemeConfig = {
         ],
       },
       {
-        href: 'https://github.com/chrononeko/chronocat',
+        href: 'https://github.com/BuDingOwO/chronocat',
         html: '<svg style="width:24px;height:24px;display:block" viewBox="0 0 24 24"><path fill="currentColor" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>',
         position: 'right',
       },
@@ -230,7 +230,7 @@ export const config: Config = {
   title,
   tagline,
 
-  url: 'https://chronocat.vercel.app',
+  url: 'https://chronocat-docs.vercel.app',
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
